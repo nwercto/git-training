141b59f167b1139b5172b3c7cd3311416fe24a3a
Tomasvlc <44083349+Tomasvlc@users.noreply.github.com>
2018-11-29 14:17:08 +0100

    test

--- Tomas.java
+++ Tomas.java
@@ -1,8 +1,13 @@
 public class Tomas {
 
 	private String nom;
+	private int age;
 
 	public Tomas (String nom){
 	this.nom =nom;
 	}
+
+	public quiSuisJe(String nom, int age)
+	this.nom = nom;
+	this.age = age;
 }
